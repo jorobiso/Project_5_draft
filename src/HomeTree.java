f2e99df852e0e45442b53f4531e6dceb78fd96b7
Bolt259 <97069761+Bolt259@users.noreply.github.com>
2023-06-14 23:13:33 -0700

    finalworking

--- src/HomeTree.java
+++ src/HomeTree.java
@@ -9,29 +9,6 @@ public class HomeTree extends Entity {
     public HomeTree(String id, Point position, List<PImage> images) {
         super(id, position, images);
     }
-//
-//    @Override
-//    public void executeActivityAction(WorldModel world, ImageStore imageStore, EventScheduler scheduler) {
-//        super.executeActivityAction(world, imageStore, scheduler);
-//
-//        if (!((Wiggler) this).transform(world, scheduler, imageStore)) {
-//
-//        }
-//    }
-//
-//    public void spawnGroot(WorldModel world, ImageStore imageStore, EventScheduler scheduler) {
-//        Point homeTreePosition = this.getPosition();
-//
-//        // Check if the spawn point is valid and unoccupied
-//        if (world.withinBounds(spawnPoint) && !world.isOccupied(spawnPoint)) {
-//            Groot groot = new Groot(Groot.getGrootKey(), spawnPoint, imageStore.getImageList(Groot.getGrootKey()),
-//            Groot.getGrootActionPeriod(), Groot.getGrootAnimationPeriod()
-//
-//            world.addEntity(groot);
-//            groot.scheduleActions(scheduler, world, imageStore);
-//        }
-//    }
-
 
     public static String getHomeTreeKey() {
         return HOME_TREE_KEY;
